4dff217cb7549e364652e718fc94ad1bf985ca5d
arman-ag <alighanbari.1994@gmail.com>
2022-01-13 16:09:25 +0330

    create project & add redux & sass to project

--- src/redux/reducer/locations.reducer.js
+++ src/redux/reducer/locations.reducer.js
@@ -1,14 +1,8 @@
-import { generalTypes } from "../../components/types/genral.types";
 const initialState = {
   locations: [{ lat: 29.5926, long: 52.5836, label: "media" }],
 };
 const location = (state = initialState, action) => {
   switch (action.type) {
-    case generalTypes.CONVERTED_XLSX:
-      return {
-        ...state,
-        locations: action?.jsonLocation,
-      };
     default:
       return state;
   }
