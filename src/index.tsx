80f9ae3fdcac739884e6745d1ade242b4e6a091b
arman-ag <alighanbari.1994@gmail.com>
2023-01-12 16:32:07 +0330

    feat: :sparkles: add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import { Provider } from 'react-redux';
 import App from './app/App';
 import './index.css';
 import { store } from './redux/store';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
 );
-// serviceWorker.register();
+serviceWorkerRegistration.register();
