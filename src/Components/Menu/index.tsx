c5c7f5e6f0c855cf594b20c362d5c5cdd4a907d3
arman-ag <alighanbari.1994@gmail.com>
2023-01-13 22:41:29 +0330

    style: :lipstick: change project name

--- src/Components/Menu/index.tsx
+++ src/Components/Menu/index.tsx
@@ -29,7 +29,7 @@ const Menu = () => {
     <div style={responsive()} className="menu">
       <div style={{ position: 'sticky', top: 0 }}>
         <div className="menu-header">
-          <span className="menu-header-app-name">Armageram</span>
+          <span className="menu-header-app-name">Armagram</span>
           <div className="search-box">
             <input type="text" />
             <button>
