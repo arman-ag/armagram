2a4e3ebba9977e1d40537d0f52476a8b0ec0dfa8
arman-ag <alighanbari.1994@gmail.com>
2023-01-23 17:25:24 +0330

    style: :lipstick: add status for contacts

--- src/Components/Header/ProfileInfo.tsx
+++ src/Components/Header/ProfileInfo.tsx
@@ -3,6 +3,7 @@ import { modalAction } from '../../redux/actions';
 import './headerStyle.scss';
 const ProfileInfo = () => {
   const profile = useSelector((state: any) => state.singleProfile.profile);
+  const messages = useSelector((state: any) => state.message);
 
   const dispatch = useDispatch();
   return (
@@ -10,7 +11,18 @@ const ProfileInfo = () => {
       <img src={profile.profileImage} alt="profile " className="profile" />
       <div className="info">
         <h3>{profile?.name}</h3>
-        <span>last seen recently</span>
+        {messages?.[profile.phone]?.message.length % 2 === 0 ? (
+          <span className="profile-status">online</span>
+        ) : (
+          <div className="typing-box">
+            <div className="typing">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <span className="typing-text">typing</span>
+          </div>
+        )}
       </div>
     </div>
   );
