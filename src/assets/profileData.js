8392e3e892a47d6113eb9d680ba7339189001ab5
arman-ag <alighanbari.1994@gmail.com>
2023-02-04 15:59:04 +0330

    refactor: :sparkles: add new contact to chat

--- src/assets/profileData.js
+++ src/assets/profileData.js
@@ -14,5 +14,29 @@ export const profileData = [
     user: 'joe_biden',
     profileImage: '/images/biden.png',
     id: 45977797
+  },
+  {
+    name: 'Gavin',
+    phone: '+2132786252',
+    bio: 'My father is an asshole. just donate me to buy a playstation ',
+    user: 'Gavin_123',
+    profileImage: '/images/funnyChild.png',
+    id: 46269752
+  },
+  {
+    name: 'Andras Arato',
+    phone: '+2132786285',
+    bio: 'I\'m the century Mona Lisa , just respect me ',
+    user: 'sadness_laugh',
+    profileImage: '/images/bigSmile.png',
+    id: 46269888
+  },
+  {
+    name: 'prefect man',
+    phone: '+2132789592',
+    bio: 'Im the perfect male that ever live on earth',
+    user: 'perfect_man',
+    profileImage: '/images/perfectMan.png',
+    id: 46269890
   }
 ];
