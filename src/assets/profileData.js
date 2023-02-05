64d8e45c8343418cf7977bf1549f040881a059d1
arman-ag <alighanbari.1994@gmail.com>
2023-02-05 11:26:03 +0330

    style: :lipstick: Change bio contact

--- src/assets/profileData.js
+++ src/assets/profileData.js
@@ -2,7 +2,7 @@ export const profileData = [
   {
     name: 'Shakira',
     phone: '+5432789512',
-    bio: 'singer & dancer.in relationship',
+    bio: 'singer & dancer.in relationship 💃',
     user: '87_shakira',
     profileImage: '/images/shakira.png',
     id: 45977698
@@ -10,7 +10,7 @@ export const profileData = [
   {
     name: 'Joe Biden',
     phone: '+2132789512',
-    bio: 'the president of USA ',
+    bio: 'the president of USA 🇺🇲 🦅 ',
     user: 'joe_biden',
     profileImage: '/images/biden.png',
     id: 45977797
@@ -18,7 +18,7 @@ export const profileData = [
   {
     name: 'Gavin',
     phone: '+2132786252',
-    bio: 'My father is an asshole. just donate me to buy a playstation ',
+    bio: 'My father is an asshole. Pleazzzzz donate me bitcoin   to buy a PlayStation 🥺',
     user: 'Gavin_123',
     profileImage: '/images/funnyChild.png',
     id: 46269752
@@ -26,7 +26,7 @@ export const profileData = [
   {
     name: 'Andras Arato',
     phone: '+2132786285',
-    bio: 'I\'m the century Mona Lisa , just respect me ',
+    bio: 'I\'m the century Mona Lisa;just respect me 💪🏻',
     user: 'sadness_laugh',
     profileImage: '/images/bigSmile.png',
     id: 46269888
@@ -34,7 +34,7 @@ export const profileData = [
   {
     name: 'prefect man',
     phone: '+2132789592',
-    bio: 'Im the perfect male that ever live on earth',
+    bio: 'I\'m the hottest perfect male that ever lives on earth 😎💪🏻',
     user: 'perfect_man',
     profileImage: '/images/perfectMan.png',
     id: 46269890
